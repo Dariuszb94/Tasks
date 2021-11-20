ff2bac83cb65f3d473daa9174e42451587f05f76
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-20 13:37:06 +0100

    person

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import "./App.css";
 let name: string;
 let age: number;
 let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+type Person = {
+  name: string;
+  age: number;
+};
+let person: Person;
+role = [5, "5"];
 function App() {
   return <div></div>;
 }
