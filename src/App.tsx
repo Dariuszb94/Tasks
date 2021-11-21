9dbd47168d6e6b4e87c0eaddcfcdc69987770a12
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-21 09:50:03 +0100

    import input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
 const App: FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <span className="heading">Taskify</span>
+      <InputField />
+    </div>
+  );
 };
 
 export default App;
