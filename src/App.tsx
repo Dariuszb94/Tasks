fcc18570957fb43b0e218d8d8a44b12fd9a47205
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-21 09:33:55 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,4 @@
 import "./App.css";
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-let any: unknown;
-// type Person = {
-//   name: string;
-//   age?: number;
-// };
-// let person: Person;
-// let lotsOfPeople: Person[];
-interface Person {
-  name: string;
-  age?: number;
-}
-type X = {
-  a: string;
-  b: number;
-};
-type Y = X & {
-  c: string;
-  d: number;
-};
-let y: Y = {
-  a: "1",
-  b: 2,
-  c: "sd",
-  d: 4,
-};
-let printName: (name: string) => void;
 
 function App() {
   return <div></div>;
