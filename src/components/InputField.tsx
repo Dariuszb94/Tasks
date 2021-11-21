a4795bb77d4002c8b221bb173816cd928549dc51
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-21 09:53:00 +0100

    input styles

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,5 +1,32 @@
+import "./styles.css";
+
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
 const InputField = () => {
-  return <div></div>;
+  return (
+    <form
+      className="input"
+      // onSubmit={(e) => {
+      //   handleAdd(e);
+      //   inputRef.current?.blur();
+      // }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Todo"
+        // value={todo}
+        // ref={inputRef}
+        // onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input__submit">
+        GO
+      </button>
+    </form>
+  );
 };
 
 export default InputField;
