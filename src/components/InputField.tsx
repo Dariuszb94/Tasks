2e1655b1670bfbf875c6f06cd83610bbec7378b9
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-24 21:05:27 +0100

    submit ref

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,3 +1,5 @@
+import React, { useRef } from "react";
+
 import "./styles.css";
 
 interface Props {
@@ -6,19 +8,21 @@ interface Props {
   handleAdd: (e: React.FormEvent) => void;
 }
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <form
       className="input"
       onSubmit={(e) => {
         handleAdd(e);
-        // inputRef.current?.blur();
+        inputRef.current?.blur();
       }}
     >
       <input
         type="text"
         placeholder="Enter a Todo"
         value={todo}
-        // ref={inputRef}
+        ref={inputRef}
         onChange={(e) => setTodo(e.target.value)}
         className="input__box"
       />
